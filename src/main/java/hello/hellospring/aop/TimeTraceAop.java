1e3c11ebc3fd479fcbc2180a8ad9f2807f252244
solbin <solbin7494@daum.net>
2022-03-24 19:00:16 +0900

    시간 측정 AOP 등록

---
+++ src/main/java/hello/hellospring/aop/TimeTraceAop.java
@@ -0,0 +1,29 @@
+package hello.hellospring.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class TimeTraceAop {
+    /*
+    원하는 곳에 공통 관심사를 지정해보자. (패키지명.클래스명..)
+     */
+    @Around("execution(* hello.hellospring..*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        System.out.println("START : " + joinPoint.toString());  // 어떤 메서드를 콜했는지 확인하기
+
+        try {
+            // 다음 메서드로 진행
+            return joinPoint.proceed();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("END : " + joinPoint.toString() + " " + timeMs + "ms");
+
+        }
+    }
+}
